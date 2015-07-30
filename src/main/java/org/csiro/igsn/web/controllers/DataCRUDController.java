97ed0addc495f632c2dda95457862eaf033b217f
Victor Tey <victor.tey@csiro.au>
2015-07-30 10:58:31 +0800

    Initial skeleton copy with 1 single controller as working example

---
+++ src/main/java/org/csiro/igsn/web/controllers/DataCRUDController.java
@@ -0,0 +1,33 @@
+package org.csiro.igsn.web.controllers;
+
+
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class DataCRUDController {
+
+    
+    @RequestMapping("test2.do")
+    public void test2(
+            @RequestParam(required = false, value ="batchid") String batchid,          
+            HttpServletResponse response)throws Exception {
+
+        response.setContentType("text/html");
+
+        
+        OutputStream outputStream = response.getOutputStream();      
+        IOUtils.write("<!DOCTYPE html><html><head><title>IGSN</title></head><body><h1>This is a example</h1></body></html>", outputStream);
+
+       
+        outputStream.close();
+    }
+
+  
+}
